f95dab6d32da618b2eecac9bf28b39e3d8205799
Beltran <Luis.Beltran@avnet.com>
2023-10-11 16:33:17 -0600

    bug_out_of_range_fix

--- Excel_GCET_APP/taskproccessdocuments.cpp
+++ Excel_GCET_APP/taskproccessdocuments.cpp
@@ -1,6 +1,8 @@
 #include "taskproccessdocuments.h"
 #include <QDebug>
 #include <QHash>
+#include <QStandardPaths>
+
 
 TaskProccessDocuments::TaskProccessDocuments(QMainWindow* window, char id, QString export_path, QList<QXlsx::Document*>* drms_document, QList<QXlsx::Document*>* booms_documents )
     : mId(id), mexport_path(export_path), mdrms_document(drms_document), mbooms_documents(booms_documents),  mainWindow(window)
@@ -22,6 +24,9 @@ void TaskProccessDocuments::run(){
     QMap<QString, QList<QVariant>> drms_columnDataMap;
     QMap<QString, QList<QVariant>> booms_columnDataMap;
     QMap<QString, QList<QVariant>> matchingDataMap;
+
+    QList<int> rowIndexes;
+
 
     qDebug() << "Export Proccess start";
 
@@ -58,20 +63,17 @@ void TaskProccessDocuments::run(){
                 drms_columnDataMap[desiredColumnName] = drms_columnData;
             }
         }
-//        // Print out with the console
-//        foreach (const QString &columnName, drms_columnDataMap.keys()) {
-//            qDebug() << "Columna:" << columnName;
-//            QList<QVariant> columnData = drms_columnDataMap[columnName];
-//            foreach (const QVariant &data, columnData) {
-//                qDebug() << data.toString();
-//            }
-//        }
+
         qDebug() << "DRMS Proccessed";
+
 
     }
     else {
         qDebug() << "The drms document have not charged correctly";
     }
+
+    // Obtener las listas de datos de las columnas que deseas comparar
+    QList<QVariant> drms_designRegistrationData = drms_columnDataMap["Design Registration Project id"];
 
 
 
@@ -79,6 +81,16 @@ void TaskProccessDocuments::run(){
     if (!mbooms_documents->isEmpty()) {
 
         for (const QXlsx::Document* boomDocument : *mbooms_documents) {
+
+            // Agregar el nombre del archivo boom
+            for (const QString &columnName : drms_columnDataMap.keys()) {
+                matchingDataMap[columnName].append(boomDocument->documentProperty("Title"));
+            }
+
+            // Agregar una fila en blanco
+            for (const QString &columnName : drms_columnDataMap.keys()) {
+                matchingDataMap[columnName].append(QVariant());
+            }
 
             QXlsx::Worksheet *booms_worksheet = boomDocument->currentWorksheet();
             QStringList booms_columns; // Store the names of the columns
@@ -110,137 +122,184 @@ void TaskProccessDocuments::run(){
                     // Almacenar los datos de la columna en el mapa
                     booms_columnDataMap[desiredColumnName] = booms_columnData;
 
-//                    // Print out with the console
-//                    foreach (const QString &columnName, booms_columnDataMap.keys()) {
-//                        qDebug() << "Columna:" << columnName;
-//                        QList<QVariant> columnData = booms_columnDataMap[columnName];
-//                        foreach (const QVariant &data, columnData) {
-//                            qDebug() << data.toString();
-//                        }
-//                    }
-
                 }
             }
 
+            // Comparative of the files
+
+            qDebug() << "Comparative Init";
+            qDebug() << "project booms : "<<"booms_projectNumberData";
+
+
+
+            QList<QVariant> booms_projectNumberData = booms_columnDataMap["Project Number"];
+
+            QList<QVariant> matchingDrmsData; // Almacenar los datos coincidentes de drms
+
+
+            // Iterar sobre los valores de "Design Registration Project id" de drms
+            for (int i = 0; i < drms_designRegistrationData.size(); ++i) {
+                const QVariant &drms_value = drms_designRegistrationData[i];
+
+                // Verificar si el valor de drms está en la lista de "Project Number" de booms
+                if (booms_projectNumberData[0] == drms_designRegistrationData[i]) {
+                    rowIndexes.append(i);
+
+
+                    // Iterar sobre las claves (nombres de las columnas) en drms_columnDataMap
+                    for (const QString &columnName : drms_columnDataMap.keys()) {
+                        // Obtener la lista de datos de la columna actual
+                        QList<QVariant> columnData = drms_columnDataMap[columnName];
+
+                        // Verificar si el índice es válido para esta columna
+                        if (i < columnData.size()) {
+                            // Obtener el valor en ese índice
+                            QVariant cellValue = columnData[i];
+
+                            // Agregar el valor al mapa de datos coincidentes
+                            matchingDataMap[columnName].append(cellValue);
+                        }
+                    }
+
+                }
+
+            }
+//            // Agregar dos filas en blanco
+//            for (int j = 0; j < 2; ++j) {
+//                for (const QString &columnName : drms_columnDataMap.keys()) {
+//                    matchingDataMap[columnName].append(QVariant());
+//                }
+//            }
+
+
+
         }
-        qDebug() << "BOOMS Proccessed";
+
+
+        // Path of the new file
+        QString desktopPath = QStandardPaths::writableLocation(QStandardPaths::DesktopLocation);
+        QString filePath = desktopPath + "/nuevo_archivo.xlsx";
+
+        QXlsx::Document xlsx;
+
+
+
+
+
+
+//        foreach (const QString &columnName, drms_columnDataMap.keys()) {
+
+//            xlsx.write(1 , colum_number, columnName);
+//            ++colum_number;
+//        }
+
+
+//        for(int i = 0; i < rowIndexes.size(); ++i){
+//            int rowIndex = rowIndexes[i];
+//            //qDebug() << "Row at index" << rowIndex << ":";
+
+
+//            foreach (const QString &columnName, drms_columnDataMap.keys()) {
+//                QList<QVariant> columnData = drms_columnDataMap[columnName];
+//                ++colum_number;
+
+
+
+//                if (rowIndexes[i] < columnData.size()) {
+//                    QVariant cellValue = columnData[rowIndexes[i]];
+//                    //qDebug() << columnName << ": " << cellValue;
+//                    xlsx.write(i + 2, colum_number, cellValue);
+//                }
+//                else {
+//                    qDebug() << "Column" << columnName << "has no data at index" << rowIndexes[i];
+//                }
+//            }
+//            //qDebug() << "------------------------";
+//        }
+
+        int rowIndex = 2; // Comenzar en la fila 2 para dejar espacio para encabezados
+
+        foreach (const QString &columnName, matchingDataMap.keys()) {
+            xlsx.write(1, matchingDataMap.keys().indexOf(columnName) + 1, columnName);
+            QList<QVariant> columnData = matchingDataMap[columnName];
+
+            for (int i = 0; i < columnData.size(); ++i) {
+                xlsx.write(rowIndex, matchingDataMap.keys().indexOf(columnName) + 1, columnData[i]);
+                ++rowIndex;
+            }
+            rowIndex = 2;
+
+        }
+
+
+
+
+
+//        int colum_number = 0;
+
+
+//        // filas
+//        for (int i = 0; i < matchingDataMap.size(); ++i) {
+
+//            // columnas
+//            for (const QString &columnName : matchingDataMap.keys()) {
+//                QList<QVariant> columnData = matchingDataMap[columnName];
+//                ++colum_number;
+
+//                QVariant cellValue = columnData[i];
+
+//                xlsx.write(i+1, colum_number, cellValue);
+
+
+//            }
+
+//        }
+
+        xlsx.setColumnWidth(1, 30);
+        xlsx.setColumnWidth(1, 30);
+        xlsx.setColumnWidth(1, 30);
+
+
+
+
+        xlsx.saveAs(filePath);
+
+        qDebug() << "Archivo Excel guardado en:" << filePath;
+
+
+
+
+        for (int i = 0; i < matchingDataMap.begin().value().size(); ++i) {
+            qDebug() << "Row" << i << ":";
+            for (const QString &columnName : matchingDataMap.keys()) {
+                QList<QVariant> columnData = matchingDataMap[columnName];
+
+                if (i < columnData.size()) {
+                    QVariant cellValue = columnData[i];
+                    qDebug() << columnName << ": " << cellValue;
+                } else {
+                    qDebug() << columnName << ": "; // Impresión de celda en blanco
+                }
+            }
+            qDebug() << "------------------------";
+        }
+
+
+
+
 
     }
-
-
 
     else {
         qDebug() << "The booms document have not charged correctly";
     }
 
 
-    // Comparative of the files
-
-    qDebug() << "Comparative Init";
-    QList<int> rowIndexes;
 
 
-    // Obtener las listas de datos de las columnas que deseas comparar
-    QList<QVariant> drms_designRegistrationData = drms_columnDataMap["Design Registration Project id"];
-    QList<QVariant> booms_projectNumberData = booms_columnDataMap["Project Number"];
-
-    QList<QVariant> matchingDrmsData; // Almacenar los datos coincidentes de drms
-
-
-    // Iterar sobre los valores de "Design Registration Project id" de drms
-    for (int i = 0; i < drms_designRegistrationData.size(); ++i) {
-        const QVariant &drms_value = drms_designRegistrationData[i];
-
-        // Verificar si el valor de drms está en la lista de "Project Number" de booms
-        if (booms_projectNumberData[1] == drms_designRegistrationData[i]) {
-            rowIndexes.append(i);
-
-            //qDebug() << "Numero en lista: " << rowIndexes[i];
-
-    }
-
-        qDebug() << "Lista de indices : " << rowIndexes;
-
-    //booms_projectNumberData(1) == drms_designRegistrationData(i)
-
-//        for(int i = 0; i < rowIndexes.size(); ++i){
-//            qDebug() << "Row at index" << rowIndexes[i] << ":";
-//            foreach (const QString &columnName, drms_columnDataMap.keys()) {
-//                QList<QVariant> columnData = drms_columnDataMap[columnName];
-
-//                if (rowIndexes[i] < columnData.size()) {
-//                    QVariant cellValue = columnData[rowIndexes[i]];
-//                    qDebug() << columnName << ": " << cellValue;
-//                }
-//                else {
-//                    qDebug() << "Column" << columnName << "has no data at index" << rowIndexes[i];
-//                }
-//            }
-//            qDebug() << "------------------------";
-//        }
-    }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-//qDebug() << "Column data: " << drms_columnDataMap[columnName];
-
-//    // Crear un nuevo objeto QXlsx::Document para el archivo Excel de salida
-//    QXlsx::Document outputExcel;
-
-//    // Iterar sobre las columnas que deseas escribir en el nuevo archivo
-//    foreach (const QString &columnName, drms_column_names_to_find) {
-//        // Obtener los datos correspondientes desde matchingDataMap
-//        QList<QVariant> columnData = matchingDataMap[columnName];
-
-//        // Escribir los datos en la nueva hoja de cálculo del archivo Excel
-//        for (int row = 0; row < columnData.size(); ++row) {
-//            outputExcel.write(row + 1, drms_column_names_to_find.indexOf(columnName) + 1, columnData[row]);
-//        }
-//    }
-
-//    // Guardar el archivo Excel en una ubicación específica
-//    outputExcel.saveAs("C:/Users/061752/OneDrive - Avnet/Desktop/outputExcel.xlsx");
-
-
-//    // Print out with the console
-//    foreach (const QString &columnName, matchingDataMap.keys()) {
-//        qDebug() << "Columna:" << columnName;
-//        QList<QVariant> columnData = matchingDataMap[columnName];
-//        foreach (const QVariant &data, columnData) {
-//            qDebug() << data.toString();
-//        }
-//    }
-
-
-
-
-
-
-//foreach (int rowIndex, rowIndexes) {
-//qDebug() << "Row at index" << rowIndex << ":";
-//foreach (const QString &columnName, matchingDataMap.keys()) {
-//    QList<QVariant> columnData = matchingDataMap[columnName];
-
-//    if (rowIndex < columnData.size()) {
-//            QVariant cellValue = columnData[rowIndex];
-//            qDebug() << columnName << ": " << cellValue;
-//    } else {
-//            qDebug() << "Column" << columnName << "has no data at index" << rowIndex;
-//    }
-//}
-//qDebug() << "------------------------";
-//}
 
 
 
