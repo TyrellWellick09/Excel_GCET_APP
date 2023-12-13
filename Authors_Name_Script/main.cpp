e13218aff65acaf8961467590a641b54b7e07bb0
Beltran, Luis <enrique.beltran@avnet.com>
2023-12-13 16:45:38 -0600

    Corrections

--- Authors_Name_Script/main.cpp
+++ Authors_Name_Script/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
     // Get the execution path
     QString execution_path = qApp->applicationDirPath();
 
-//    qDebug() << "App path : " << qApp->applicationDirPath();
+    //    qDebug() << "App path : " << qApp->applicationDirPath();
 
     // Get the filespath
     QString currentPath = execution_path;
@@ -30,8 +30,8 @@ int main(int argc, char *argv[]) {
     // get the path
     QString pathFiles = dir.absolutePath();
 
-//    qDebug() << "Path original: " << currentPath;
-//    qDebug() << "Path anterior: " << pathFiles;
+    //    qDebug() << "Path original: " << currentPath;
+    //    qDebug() << "Path anterior: " << pathFiles;
 
     QStringList excelFiles;  // Store the excel files
 
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
         excelFiles.append(it.next());  // add the path to the list
     }
 
-//    qDebug() << "Archivos Excel encontrados:";
+    //    qDebug() << "Archivos Excel encontrados:";
     foreach (const QString &file, excelFiles) {
         qDebug() << file;
     }
@@ -53,14 +53,10 @@ int main(int argc, char *argv[]) {
     // Toma los últimos 5 caracteres del nombre de la PC
     QString username = nombrePC.right(5);
 
-//    qDebug() << "Nombre de la PC: " << nombrePC;
-//    qDebug() << "Últimos 5 caracteres del nombre de la PC: " << username;
+    //    qDebug() << "Nombre de la PC: " << nombrePC;
+    //    qDebug() << "Últimos 5 caracteres del nombre de la PC: " << username;
 
-
-
-
-
-        foreach (QString const &excelPathfile, excelFiles) {
+    foreach (QString const &excelPathfile, excelFiles) {
 
         // Verify is the file exist
         QFile file(excelPathfile);
@@ -72,10 +68,21 @@ int main(int argc, char *argv[]) {
             QString nombreBase = fileInfo.completeBaseName();
             QString extension = fileInfo.suffix();
 
-                if(nombreBase.right(5) != username){
+            if (nombreBase.contains(username)) {
+                // Remove the username from the file name
+                nombreBase.remove("_"+username);
 
+                // Create the new file name without the username
+                QString nuevoNombreArchivo = QString("%1.%2").arg(nombreBase, extension);
 
-
+                // Rename the file
+                QFile nuevoFile(rutaArchivo + "/" + nuevoNombreArchivo);
+                if (file.rename(nuevoFile.fileName())) {
+                    qDebug() << "The username has been removed from the file name. The file is now:" << nuevoFile.fileName();
+                } else {
+                    qDebug() << "Error renaming the file.";
+                }
+            } else if(nombreBase.right(5) != username){
                 // Create the new number with the username
                 QString nuevoNombreArchivo = QString("%1_%2.%3").arg(nombreBase, username, extension);
 
@@ -86,26 +93,15 @@ int main(int argc, char *argv[]) {
                 } else {
                     qDebug() << "Error al renombrar el archivo.";
                 }
-            }
-            else{qDebug() << "The file already has the username";
-                openConsole = true;
 
             }
-        } else {
-            qDebug() << "El archivo no existe.";
-            openConsole = true;
+
+            if(openConsole == false){a.exit();}
+            else{return a.exec();}
+
+
+
         }
-        }
-
-
-
-    if(openConsole == false){a.exit();}
-    else{return a.exec();}
-
-
-
+    }
 }
-
-
-
 
